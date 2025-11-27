d568a38dcfaa2c40090ceec63a7e5d1be11e39b7
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2025-11-27 16:27:03 -0300

    feat: configurando o banco de dados na aplicação.

--- config/config/settings.py
+++ config/config/settings.py
@@ -74,8 +74,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'biblioteca_db',
+        'USER': 'root',
+        'PASSWORD': 'senai2025',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", # sql mode ativo para não aceitar dados inválidos.
+        },
     }
 }
 
